7bf40395c2f76f8cc85fcbf0f25aded6cafa6092
Ryanhernawan <ryan.hernawan1508@gmail.com>
2022-04-25 13:25:27 +0700

    update Mini Project

---
+++ MiniProject-RyanHernawan/src/test/java/com/alterra/MiniProjectRyanHernawan/MiniProjectRyanHernawanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alterra.MiniProjectRyanHernawan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectRyanHernawanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
